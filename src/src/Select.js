784caa875d8e062131244cc3f92640d26c38b7be
Mathéo Miquel <matheomiquel@MacBook-Air-de-Matheo.local>
2021-10-18 16:53:54 +0200

    episode with material ui

--- src/src/Select.js
+++ src/src/Select.js
@@ -3,23 +3,32 @@ import { useEffect,useState } from 'react';
 import { DataGrid } from '@mui/x-data-grid';
 function Select() {
     const [episode, setEpisode] = useState("");
+    const [test, setTest] = useState([])
+    useEffect(async () => {
+        getEpisodes()
+      },[]);
     async function getEpisodes() {
         const episodes = await axios.get("https://rickandmortyapi.com/api/episode")
-        const episodesFilter = episodes.data.results.map(episode => {
-            return (
-                    <p>
-                      {episode.episode} {episode.name}
-                    </p>
-              );
+        const episodesFilter = episodes.data.results.map((episode,index) => {
+            return { episode : episode.episode, name : episode.name, id: index}
         })
-        setEpisode(episodesFilter)
-        console.log("here")
-        return episodesFilter
+        setTest(episodesFilter)
+        console.log(test)
     };
-    getEpisodes()
+
+    const columns = [
+        {field: 'episode', headerName: 'episode',width: 150},
+         {field: 'name', headerName: 'Name'}
+    ]
     return (
-      <div className="Select">
-          {episode}
+        <div style={{ height: 400, width: '100%' }}>
+         <DataGrid
+  rows={test}
+  columns={columns}
+  pageSize={5}
+  rowsPerPageOptions={[5]}
+  checkboxSelection
+/>
       </div>
     );
   }
